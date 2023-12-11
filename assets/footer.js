f8c346fb28a23233db488b64165f5d439bfa79e8
Patrick Cheng <pcheng86@gmail.com>
2023-12-10 22:44:14 -0800

    flow done

--- assets/footer.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.getElementById('footer').innerHTML = '<nav class="social-media-navbar"><ul> \
-            <li><a href="https://www.linkedin.com/in/swepat" target="_blank" class="flex flex-col items-center"><i class="fa fa-github mx-8"></i><span class="text-xs mt-2">git</span></a></li> \
-            <li><a href="https://www.instagram.com/alpinewizard" target="_blank" class="flex flex-col items-center"><i class="fa fa-instagram mx-8"></i><span class="text-xs mt-2">insta</span></a></li> \
-            <li><a href="https://github.com/chengp3" target="_blank" class="flex flex-col items-center"><i class="fa fa-linkedin mx-8"></i><span class="text-xs mt-2">linkedin</span></a></li> \
-        </ul></nav>';
-});

b8dee46a377cf1e8d81725de8ca0da0a3ea5746a
Patrick Cheng <pcheng86@gmail.com>
2024-01-03 11:06:12 -0800

    skyhook

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { useId } from 'react';
 import { motion } from 'framer-motion'
 import AnimatedContainer from '../components/Animate/AnimatedContainer'
 import AnimatedDiv from '../components/Animate/AnimatedDiv'
+import Link from 'next/link'
 
 export default function Home() {
   const container = {
@@ -26,35 +27,43 @@ export default function Home() {
   }
 
   return (
-    <main className="max-w-sm md:max-w-xl my-10 flex flex-col items-center transition-swipe px-6 flex-grow">
-      <div id="content" className="flex flex-col w-full items-center justify-center saturate-100 drop-shadow-2xl transition-swipe">
-        <div className="display sm:hidden w-full">
-          <a href="https://www.wta.org/go-hiking/hikes/mailbox-peak"
-            className="flex justify-center"><img src="https://patslookout.s3.us-west-2.amazonaws.com/mailbox.jpg"
-              className="saturate-100 drop-shadow-2xl w-1/2 my-4"
-              alt="On a clear day..." /></a>
+    <main className="max-w-sm md:max-w-2xl my-10 flex flex-col items-center transition-swipe px-6 flex-grow">
+      <div id="index-content" className="saturate-100 drop-shadow-2xl">
+        <div id="bio" className='md:w-4/5'>
+          <div className="display sm:hidden w-full">
+            <a href="https://www.wta.org/go-hiking/hikes/mailbox-peak"
+              className="flex justify-center"><img src="https://patslookout.s3.us-west-2.amazonaws.com/mailbox.jpg"
+                className="saturate-100 drop-shadow-2xl w-1/2 my-4"
+                alt="On a clear day..." /></a>
+          </div>
+          <AnimatedContainer className="w-full">
+            <motion.a key='1' href="https://www.wta.org/go-hiking/hikes/mailbox-peak"
+              variants={item}
+            >
+              <img key={useId()} 
+                src="https://patslookout.s3.us-west-2.amazonaws.com/mailbox.jpg" 
+                className="float-right saturate-100 drop-shadow-2xl w-1/4 my-4 mx-8
+                        hidden md:block"
+                alt="On a clear day..." />
+            </motion.a>
+            <AnimatedDiv className='my-4'>My name is Patrick 👋🏻 I solve problems with software. This website is part portfolio and part workshop/lab where I host personal projects.</AnimatedDiv>
+            <AnimatedDiv className='my-4'>A little about me. I was born in Austin, Texas, and then discovered a love for mountains; now I currently live in Portland, OR.</AnimatedDiv>
+            <AnimatedDiv className='my-4'>Software-wise, (chronologically) I have been a financial data engineer for the SEC using Hadoop-based pipelines. I've used scikitlearn machine learning algorithms to diagnose leukemia. Lately I have been doing full stack web development for <a href="https://www.galacticpolymath.com"><b>Galactic Polymath</b></a>. Mostly NextJS-based stuff.
+            </AnimatedDiv>
+            <AnimatedDiv className='my-4'>The picture above is from the summit of Mailbox Peak near Seattle, WA, and inspired the domain name for this site. That's Mount Rainier in the distance! I will incorporate a trip journal and gallery here soon.
+            </AnimatedDiv>
+            
+          </AnimatedContainer>
         </div>
-        <AnimatedContainer className="w-full">
-          <motion.a key='1' href="https://www.wta.org/go-hiking/hikes/mailbox-peak"
-            variants={item}
-          >
-            <img key={useId()} 
-              src="https://patslookout.s3.us-west-2.amazonaws.com/mailbox.jpg" 
-              className="float-right saturate-100 drop-shadow-2xl w-1/4 my-4 mx-8
-                      hidden md:block"
-              alt="On a clear day..." />
-          </motion.a>
-          <AnimatedDiv className='my-4'>My name is Patrick 👋🏻 I solve problems with software. This website is part portfolio and part workshop/lab where I host personal projects.</AnimatedDiv>
-          <AnimatedDiv className='my-4'>A little about me. I was born in Austin, Texas, and then discovered a love for mountains; now I currently live in Portland, OR.</AnimatedDiv>
-          <AnimatedDiv className='my-4'>Software-wise, (chronologically) I have been a financial data engineer for the SEC using Hadoop-based pipelines. I've used scikitlearn machine learning algorithms to diagnose leukemia. Lately I have been doing full stack web development for <a href="https://www.galacticpolymath.com"><b>Galactic Polymath</b></a>. Mostly NextJS-based stuff.
-          </AnimatedDiv>
-          <AnimatedDiv className='my-4'>The picture above is from the summit of Mailbox Peak near Seattle, WA, and inspired the domain name for this site. That's Mount Rainier in the distance! I will incorporate a trip journal and gallery here soon.
-          </AnimatedDiv>
-          <AnimatedDiv className='my-4'>Change-log:
-            <li>Dec 27, '23: Portland Area Gyms page v1</li>
-            <li>Dec 24, '23: Fixed climbing page hover bugs. Added more pics (including twice as many dogs). Fixed margins on Bat Wall.</li>
-          </AnimatedDiv>
-        </AnimatedContainer>
+        <div id="changelog">
+          <AnimatedContainer className='my-4'>
+            <AnimatedDiv className='header2'>Change Log</AnimatedDiv>
+            <AnimatedDiv className='change-item'><b>Dec 27, '23:</b> 
+            <br/><Link className='link' href="/portlandgyms">An Unsolicited Guide</Link> to Portland Area Gyms</AnimatedDiv>
+            <AnimatedDiv className='change-item'><b>Dec 24, '23:</b> 
+            <br/>100% More Dog <Link className='link' href="/photos">Photos</Link></AnimatedDiv>
+          </AnimatedContainer>
+        </div>
       </div>
     </main>
   )
